0ff6482b011d83e1b28bdca9edd01198a3952484
LeticiaBeatrizz <b.freire@escolar.ifrn.edu.br>
2026-07-15 03:29:26 -0300

    Delete dados directory

--- dados/banco.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-
-DATABASE_URL = "mysql+mysqlconnector://root:labinfo@127.0.0.1:3306/aplicacao"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
